edf1222e15c6b66fe806de773b4e97bcbff12dea
Marcin Szalek <marcin.szalek1@gmail.com>
2017-12-06 06:53:22 +0100

    added dummy widget test

--- test/dummy_test.dart
+++ test/dummy_test.dart
@@ -1,8 +1,28 @@
-import 'package:test/test.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:test/test.dart' as unitTest;
+import 'package:weight_tracker/main.dart';
 
 void main() {
   test('my first unit test', () {
     var answer = 42;
-    expect(answer, 42);
+    unitTest.expect(answer, 42);
   });
+
+  testWidgets('my first widget test', (WidgetTester tester) async {
+    // You can use keys to locate the widget you need to test
+    var sliderKey = new UniqueKey();
+    var value = 0.0;
+
+    // Tells the tester to build a UI based on the widget tree passed to it
+    await tester.pumpWidget(
+      new StatefulBuilder(
+        builder: (BuildContext context, StateSetter setState) {
+          return new MyApp();
+        },
+      ),
+    );
+    expect(value, equals(0.0));
+  });
+
 }
